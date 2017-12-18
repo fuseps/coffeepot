7ff158ffd11acad77d6ab0b69d07663e77c6c0e3
Unknown <paul.sohi@autodesk.com>
2017-12-18 15:33:42 +0000

    work in progree file for the code

---
+++ WIPot.py
@@ -0,0 +1,14 @@
+import sys
+## This section will add the time libraries
+from PIL import ImageFont
+import datetime
+import time
+import calendar
+import icalendar
+import datetime
+
+##This will add the inkyphat library
+import inkyphat
+
+print("""This code will make your coffee pot an iot pot! Check at LINK TO BE ADDED SOON
+for the laser cutting files)
